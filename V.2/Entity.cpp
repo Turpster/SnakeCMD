873826ee25294f80306f656a53ea69d753bcfba6
Turpster <25931565+Turpster@users.noreply.github.com>
2018-07-18 02:29:58 +0100

    Added Boundaries

--- V.2/Entity.cpp
+++ V.2/Entity.cpp
@@ -1,13 +1,14 @@
 #include "Entity.h"
 #include "Location.h"
+#include "Game.h"
 #include <iostream>
 
 Entity::Entity(const Location& loc, const Vector& vec, const char& model) : loc(loc), vec(vec), model(model) {}
 
 void Entity::tick()
 {
-	loc.x += vec.xv;
-	loc.y += vec.yv;
+	if ((loc.x + vec.xv + 1 < Game::WIDTH && loc.x + vec.xv > 0)) loc.x += vec.xv;
+	if ((loc.y + vec.yv + 1 < Game::HEIGHT && loc.y + vec.yv > 0)) loc.y += vec.yv;
 }
 
 void Entity::render(int x, int y)
