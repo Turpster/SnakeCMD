30b575ca74f8fb2f890139ae8f414e4158ab522f
turpster <www-reece--10-1@hotmail.com>
2018-07-22 05:22:57 +0100

    Started on c# snake

---
+++ SnakeCS/Handler.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Threading;
+
+public class Handler
+{
+
+}
--- SnakeCS/Program.cs
+++ SnakeCS/Program.cs
@@ -1,12 +1,13 @@
 using System;
-
 namespace SnakeCS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue . . . ");
             Console.ReadKey();
         }
     }
